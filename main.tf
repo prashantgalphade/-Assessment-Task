f1dbe1a95a84e6a3a2390d51f5eeeb94aef5b4f7
prashantgalphade <160693778+prashantgalphade@users.noreply.github.com>
2025-02-12 10:29:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_eks_cluster" "eks_cluster" {
-  name     = "my-eks-cluster"
+  name     = "assessment-cluster"
   role_arn = aws_iam_role.eks_role.arn
   vpc_config {
     subnet_ids = ["subnet-123", "subnet-456"]
