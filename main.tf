e7334d13a4f52ab42f27a16985b6e4bb877f692c
prashantgalphade <160693778+prashantgalphade@users.noreply.github.com>
2025-02-12 12:46:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_eks_cluster" "eks_cluster" {
   name     = "assessment-cluster"
 
   vpc_config {
-    subnet_ids = ["subnet-0a1b2c3d4e5f06182", "subnet-02376afcbef0"]
+    subnet_ids = ["subnet-0a1b2c3d4e5f06182", "subnet-0a1b2c3d4e5f6af190"]
   }
 
  tags = {
